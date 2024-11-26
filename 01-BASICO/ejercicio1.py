f5a11466b25cd643cd06e279fe28592f265db892
JeO <jeo20@hotmail.com>
2024-11-26 11:40:26 -0300

    poo repaso

--- 01-BASICO/ejercicio1.py
+++ 01-BASICO/ejercicio1.py
@@ -1,35 +1,22 @@
-"""
-Crear una clase Estudiante con los atributos Nombre, Edad y Grado.
-Crear el metodo estudiar() que muestre el estudiante (nombre) esta estudiando
-Crear un objeto Estudiante y usar el metodo estudiar()
-Se debe interactuar con el usuario y este debe brindar los atributos
-Al escribir estudiar utilizar el metodo estudiar() no case sensitive
-"""
-
-
-class Estudiante():
+class Estudiante:
     def __init__(self, nombre, edad, grado):
         self.nombre = nombre
         self.edad = edad
         self.grado = grado
-
+        
     def estudiar(self):
-        print("Estudiando...")
+        print('estudiando...')
+        
+nombre = input('Ingrese el nombre: ')
+edad = input('Ingrese la edad: ')
+grado = input('Ingrese el grado: ')
 
+estudiante = Estudiante(nombre, edad, grado)
 
-nombre_estudiante = input("Ingrese el nombre: ")
-edad_estudiante = input("Ingrese la edad: ")
-grado_estudiante = input("Ingrese el grado: ")
-
-estudiante1 = Estudiante(nombre_estudiante, edad_estudiante, grado_estudiante)
-print(f"""
-      Datos del Estudiante: \n
-      Nombre: {estudiante1.nombre} \n
-      Edad: {estudiante1.edad} \n
-      Grado: {estudiante1.grado} \n
-      """)
+print(f"El estudiante se llama {estudiante.nombre}")
 
 while True:
     estudiar = input()
     if (estudiar.lower() == "estudiar"):
-        estudiante1.estudiar()
+        estudiante.estudiar()
+
